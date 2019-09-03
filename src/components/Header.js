aa5d9206c75c0ef960fb206593d0ed630d1c169c
Trung Nguyen <ntt2k@Trungs-MBP.attlocal.net>
2019-09-03 09:29:54 -0500

    setup routing and chart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import styled from 'styled-components';
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
 import { faChartArea, faCog } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'shards-react';
@@ -9,27 +11,32 @@ const IconStyled = styled(FontAwesomeIcon)`
   margin-right: 6px;
 `;
 
-const Header = () => {
+const Header = ({ location }) => {
   return (
     <header>
       <Navbar>
-        <NavbarBrand href="/">
+        <NavbarBrand>
           <img src={logo} alt="logo" />
           React
         </NavbarBrand>
 
         <Nav pills>
           <NavItem>
-            <NavLink active href="/">
-              <IconStyled icon={faChartArea} />
-              Dashboard
-            </NavLink>
+            <Link to="/">
+              <NavLink active={location.pathname === '/'}>
+                <IconStyled icon={faChartArea} />
+                Dashboard
+              </NavLink>
+            </Link>
           </NavItem>
+
           <NavItem>
-            <NavLink href="/settings">
-              <IconStyled icon={faCog} />
-              Settings
-            </NavLink>
+            <Link to="/settings">
+              <NavLink active={location.pathname === '/settings'}>
+                <IconStyled icon={faCog} />
+                Settings
+              </NavLink>
+            </Link>
           </NavItem>
         </Nav>
       </Navbar>
@@ -37,4 +44,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default withRouter(Header);
