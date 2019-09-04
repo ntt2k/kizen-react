b95ed564e143a551749e7bf96abe563b3f372c7d
Trung Nguyen <ntt2k@Trungs-MBP.attlocal.net>
2019-09-03 20:18:14 -0500

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import INITIAL_STATE from './initialstate';
+
+const initialState = INITIAL_STATE;
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
