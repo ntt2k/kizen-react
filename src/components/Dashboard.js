77c1857cc974b2b4d55c91833d1af9ba6051923f
Trung Nguyen <ntt2k@Trungs-MBP.attlocal.net>
2019-09-03 19:18:11 -0500

    data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -38,7 +38,7 @@ const Dashboard = ({ chartData }) => (
     {/* Big Stats Blocks */}
     <Row>
       <Col lg="12" md="12" sm="12" className="mb-4">
-        <BigStats />
+        <BigStats chartData={chartData} />
       </Col>
     </Row>
   </Container>
@@ -51,8 +51,9 @@ Dashboard.propTypes = {
 Dashboard.defaultProps = {
   chartData: [
     {
+      id: 'btc',
       label: 'Bitcoin (BTC)',
-      value: '17,281',
+      value: '15,281',
       percentage: '2.4%',
       increase: false,
       decrease: true,
@@ -60,18 +61,54 @@ Dashboard.defaultProps = {
       attrs: { md: '4', sm: '6' },
       datasets: [
         {
-          label: 'Today',
+          label: 'Bitcoin (BTC)',
           fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: 'rgb(0,123,255,0.1)',
-          borderColor: 'rgb(0,123,255)',
-          data: [3, 2, 3, 2, 4, 5, 4]
+          borderColor: 'rgb(255,65,105)',
+          backgroundColor: 'rgba(255,65,105,0.1)',
+          pointBackgroundColor: '#ffffff',
+          pointHoverBackgroundColor: 'rgb(0,123,255)',
+          pointRadius: 0,
+          pointHoverRadius: 3,
+          data: [
+            15000,
+            18000,
+            13200,
+            11800,
+            12400,
+            13200,
+            12300,
+            16500,
+            15900,
+            12000,
+            17500,
+            19400,
+            14200,
+            12000,
+            10600,
+            12500,
+            9820,
+            12800,
+            12102,
+            11200,
+            13200,
+            13202,
+            14000,
+            12800,
+            13200,
+            13200,
+            13400,
+            12910,
+            13100,
+            14250
+          ]
         }
       ]
     },
     {
+      id: 'bch',
       label: 'Bitcoin Cash (BCH)',
-      value: '8,147',
+      value: '4800',
       percentage: '3.8%',
       increase: false,
       decrease: true,
@@ -79,48 +116,157 @@ Dashboard.defaultProps = {
       attrs: { md: '4', sm: '6' },
       datasets: [
         {
-          label: 'Today',
+          label: 'Bitcoin Cash (BCH)',
           fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: 'rgba(255,180,0,0.1)',
           borderColor: 'rgb(255,180,0)',
-          data: [2, 3, 3, 3, 4, 3, 3]
+          backgroundColor: 'rgba(255,180,0,0.1)',
+          pointBackgroundColor: '#ffffff',
+          pointHoverBackgroundColor: 'rgba(255,65,105,1)',
+          pointRadius: 0,
+          pointHoverRadius: 2,
+          pointBorderColor: 'rgba(255,65,105,1)',
+          data: [
+            980,
+            1200,
+            640,
+            360,
+            480,
+            640,
+            460,
+            1000,
+            980,
+            1100,
+            1050,
+            880,
+            840,
+            940,
+            920,
+            900,
+            840,
+            700,
+            1024,
+            840,
+            940,
+            1104,
+            1280,
+            2400,
+            1200,
+            5800,
+            2400,
+            2220,
+            2000,
+            1800
+          ]
         }
       ]
     },
     {
+      id: 'eth',
       label: 'Ethereum (ETH)',
-      value: '2,390',
-      percentage: '4.7%',
+      value: '4,790',
+      percentage: '10.7%',
       increase: true,
       chartLabels: [null, null, null, null, null, null, null],
       attrs: { md: '6', sm: '6' },
       datasets: [
         {
-          label: 'Today',
+          label: 'Ethereum (ETH)',
           fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: 'rgba(0, 184, 216, 0.1)',
-          borderColor: 'rgb(0, 184, 216)',
-          data: [1, 2, 1, 3, 5, 4, 7]
+          borderColor: 'rgb(0,123,255)',
+          backgroundColor: 'rgb(0,123,255,0.1)',
+          pointBackgroundColor: '#ffffff',
+          pointHoverBackgroundColor: 'rgb(0,123,255)',
+          pointRadius: 0,
+          pointHoverRadius: 3,
+          data: [
+            500,
+            800,
+            320,
+            180,
+            240,
+            320,
+            230,
+            650,
+            590,
+            1200,
+            750,
+            940,
+            1420,
+            1200,
+            960,
+            1450,
+            1820,
+            2800,
+            2102,
+            1920,
+            3920,
+            3202,
+            3140,
+            2800,
+            3200,
+            3200,
+            3400,
+            2910,
+            3100,
+            4250
+          ]
         }
       ]
     },
     {
+      id: 'ltc',
       label: 'Litecoin (LTC)',
-      value: '182',
-      percentage: '12.4',
+      value: '1182',
+      percentage: '5.4%',
       increase: true,
       chartLabels: [null, null, null, null, null, null, null],
       attrs: { md: '6', sm: '6' },
       datasets: [
         {
-          label: 'Today',
+          label: 'Litecoin (LTC)',
           fill: 'start',
           borderWidth: 1.5,
-          backgroundColor: 'rgba(23,198,113,0.1)',
           borderColor: 'rgb(23,198,113)',
-          data: [1, 2, 3, 3, 3, 4, 4]
+          backgroundColor: 'rgba(23,198,113,0.1)',
+          pointBackgroundColor: '#ffffff',
+          pointHoverBackgroundColor: 'rgba(255,65,105,1)',
+          pointRadius: 0,
+          pointHoverRadius: 2,
+          pointBorderColor: 'rgba(255,65,105,1)',
+          data: [
+            380,
+            430,
+            120,
+            230,
+            410,
+            740,
+            472,
+            219,
+            391,
+            229,
+            400,
+            203,
+            301,
+            380,
+            291,
+            620,
+            700,
+            300,
+            630,
+            402,
+            320,
+            380,
+            289,
+            410,
+            300,
+            530,
+            630,
+            720,
+            780,
+            1200
+          ]
         }
       ]
     }
--- src/components/utils/BigStats.js
+++ src/components/utils/BigStats.js
@@ -67,96 +67,7 @@ class BigStats extends React.Component {
 
     const formattedChartData = {
       labels: Array.from(new Array(30), (_, i) => (i === 0 ? 1 : i)),
-      datasets: [
-        {
-          label: 'Current Month',
-          fill: 'start',
-          data: [
-            500,
-            800,
-            320,
-            180,
-            240,
-            320,
-            230,
-            650,
-            590,
-            1200,
-            750,
-            940,
-            1420,
-            1200,
-            960,
-            1450,
-            1820,
-            2800,
-            2102,
-            1920,
-            3920,
-            3202,
-            3140,
-            2800,
-            3200,
-            3200,
-            3400,
-            2910,
-            3100,
-            4250
-          ],
-          backgroundColor: 'rgba(0,123,255,0.1)',
-          borderColor: 'rgba(0,123,255,1)',
-          pointBackgroundColor: '#ffffff',
-          pointHoverBackgroundColor: 'rgb(0,123,255)',
-          borderWidth: 1.5,
-          pointRadius: 0,
-          pointHoverRadius: 3
-        },
-        {
-          label: 'Past Month',
-          fill: 'start',
-          data: [
-            380,
-            430,
-            120,
-            230,
-            410,
-            740,
-            472,
-            219,
-            391,
-            229,
-            400,
-            203,
-            301,
-            380,
-            291,
-            620,
-            700,
-            300,
-            630,
-            402,
-            320,
-            380,
-            289,
-            410,
-            300,
-            530,
-            630,
-            720,
-            780,
-            1200
-          ],
-          backgroundColor: 'rgba(255,65,105,0.1)',
-          borderColor: 'rgba(255,65,105,1)',
-          pointBackgroundColor: '#ffffff',
-          pointHoverBackgroundColor: 'rgba(255,65,105,1)',
-          borderDash: [3, 3],
-          borderWidth: 1,
-          pointRadius: 0,
-          pointHoverRadius: 2,
-          pointBorderColor: 'rgba(255,65,105,1)'
-        }
-      ]
+      datasets: this.props.chartData.map(i => i.datasets ? i.datasets[0] : {})
     };
 
     const CryptoBigChart = new Chart(this.canvasRef.current, {
@@ -165,7 +76,6 @@ class BigStats extends React.Component {
       data: formattedChartData
     });
 
-    // They can still be triggered on hover.
     const buoMeta = CryptoBigChart.getDatasetMeta(0);
     buoMeta.data[0]._model.radius = 0;
     buoMeta.data[formattedChartData.datasets[0].data.length - 1]._model.radius = 0;
@@ -196,7 +106,7 @@ BigStats.propTypes = {
   /**
    * The chart dataset.
    */
-  chartData: PropTypes.object
+  chartData: PropTypes.array
 };
 
 export default BigStats;
